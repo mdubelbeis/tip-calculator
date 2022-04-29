96f40e194d0dc0871caf1bdbb5a8ddd9cccda1aa
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-29 16:27:37 -0400

    set custom colors/font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
+  content: ['./src/**/*.{js,jsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      'strong-cyan': 'hsl(172, 67%, 45%)',
+      'very-dark-cyan': 'hsl(183, 100%, 15%)',
+      'dark-grayish-cyan': 'hsl(186, 14%, 43%)',
+      'grayish-cyan': 'hsl(184, 14%, 56%)',
+      'light-grayish-cyan': 'hsl(185, 41%, 84%)',
+      'very-light- gray - cyan': 'hsl(189, 41%, 97%)',
+      'pure-white': 'hsl(0, 0%, 100%)',
+    },
+    fontFamily: {
+      'space-mono': ['Space Mono', 'monospace'],
+    },
   },
   plugins: [],
 };
