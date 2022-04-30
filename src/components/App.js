20387f2e95dd4a955dc6b83707288a574fc403cd
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-30 00:23:53 -0400

    styled for desktop

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,14 @@ import Output from './Output';
 
 const App = () => {
   return (
-    <div className="flex flex-col h-screen items-center">
-      <header className="p-8 text-center font-semibold  tracking-[8px]">
+    <div className="flex flex-col h-screen items-center xl:justify-center">
+      <header className="p-8 text-center font-semibold tracking-[8px] xl:p-20">
         <div className="text-very-dark-cyan text-xl">
           <p>SPLI</p>
           <p>TTER</p>
         </div>
       </header>
-      <main className="w-screen h-screen p-8 bg-pure-white rounded-t-3xl max-w-[500px]">
+      <main className="w-screen p-8 bg-pure-white rounded-t-3xl max-w-[500px] xl:grid xl:grid-cols-2 xl:gap-10 xl:max-w-[920px] xl:max-h-[481px]">
         <Input />
         <Output />
       </main>
