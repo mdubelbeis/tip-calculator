781461b14f802846a2df38c66e5bc5f7ef76e8c1
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-29 23:59:29 -0400

    styled mobile

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,28 +1,28 @@
-const Output = () => {
+const Output = ({ tip, total }) => {
   return (
-    <div>
-      <div>
+    <div className="mt-8 bg-very-dark-cyan py-6 px-6 space-y-8 rounded-2xl">
+      <div className="mt-2 flex justify-between">
         <div>
-          <h3>Tip Amount</h3>
-          <p>/ person </p>
+          <h3 className="text-pure-white">Tip Amount</h3>
+          <p className="text-dark-grayish-cyan text-sm ">/ person </p>
         </div>
         <div>
-          <p>TIP PER PERSON</p>
-        </div>
-      </div>
-
-      <div>
-        <div>
-          <h3>Total</h3>
-          <p>/ person </p>
-        </div>
-        <div>
-          <p>Total Per Person</p>
+          <p className="text-strong-cyan text-2xl">{`$${tip}`}</p>
         </div>
       </div>
 
-      <div>
-        <button>Reset</button>
+      <div className="flex justify-between">
+        <div>
+          <h3 className="text-pure-white">Total</h3>
+          <p className="text-dark-grayish-cyan text-sm ">/ person </p>
+        </div>
+        <div>
+          <p className="text-strong-cyan text-2xl">{`$${tip}`}</p>
+        </div>
+      </div>
+
+      <div className="text-center w-full bg-strong-cyan text-lg rounded-md py-2">
+        <button>RESET</button>
       </div>
     </div>
   );
