00ea98c857b66832697edfc136dea67d0c204403
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-30 14:10:17 -0400

    resized output text

--- src/components/Output.js
+++ src/components/Output.js
@@ -2,27 +2,27 @@ const Output = ({ tipPerPerson, totalPerPerson, resetValues }) => {
   return (
     <div className="mt-8 bg-very-dark-cyan py-6 px-6 space-y-8 flex flex-col justify-between rounded-2xl xl:mt-0 xl:space-y-0 xl:p-8">
       <div className="space-y-6 xl:space-y-12">
-        <div className="mt-2 flex justify-between flex-wrap">
+        <div className="mt-2 flex justify-between items-center flex-wrap">
           <div>
-            <h3 className="text-pure-white">Tip Amount</h3>
+            <span className="text-pure-white">Tip Amount</span>
             <p className="text-dark-grayish-cyan text-sm ">
               / person{' '}
             </p>
           </div>
           <div>
-            <p className="text-strong-cyan text-4xl font-semibold">{`$${tipPerPerson}`}</p>
+            <p className="text-strong-cyan text-6xl font-semibold">{`$${tipPerPerson}`}</p>
           </div>
         </div>
 
-        <div className="flex justify-between flex-wrap">
+        <div className="flex justify-between items-center flex-wrap">
           <div>
-            <h3 className="text-pure-white">Total</h3>
+            <span className="text-pure-white">Total</span>
             <p className="text-dark-grayish-cyan text-sm ">
               / person{' '}
             </p>
           </div>
           <div>
-            <p className="text-strong-cyan text-4xl font-semibold">{`$${totalPerPerson}`}</p>
+            <p className="text-strong-cyan text-6xl font-semibold">{`$${totalPerPerson}`}</p>
           </div>
         </div>
       </div>
