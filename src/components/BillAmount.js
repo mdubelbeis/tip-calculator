fcf6472ed59fe4488d14fbcd6f20eb0904c5fd41
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-29 23:18:20 -0400

    styled mobile

--- src/components/BillAmount.js
+++ src/components/BillAmount.js
@@ -1,16 +1,29 @@
 import { useState } from 'react';
 
+import dollar from '../images/icon-dollar.svg';
+
 const BillAmount = () => {
   const [bill, setBill] = useState(0);
+
   return (
     <form>
       <label htmlFor="bill-amount">
-        <input
-          type="text"
-          value={bill}
-          id="bill-amount"
-          onChange={(e) => setBill(e.target.value)}
-        />
+        Bill
+        <div className="relative">
+          <input
+            className="relative w-full p-4 bg-very-light-gray-cyan mt-2 focus:outline-0 text-right text-very-dark-cyan font-semibold text-2xl"
+            type="text"
+            value={bill}
+            id="bill-amount"
+            onChange={(e) => setBill(e.target.value)}
+            onClick={(e) => setBill('')}
+          />
+          <img
+            className="absolute top-[34px] left-4"
+            src={dollar}
+            alt="dollar sign"
+          />
+        </div>
       </label>
     </form>
   );
