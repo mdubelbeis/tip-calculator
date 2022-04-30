452e327000b291b728efc08e9f98f590b7a6bd53
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-30 14:01:42 -0400

    changed location of label

--- src/components/BillAmount.js
+++ src/components/BillAmount.js
@@ -3,24 +3,25 @@ import dollar from '../images/icon-dollar.svg';
 const BillAmount = ({ setBill, billAmount }) => {
   return (
     <form onSubmit={(e) => e.preventDefault()}>
-      <label htmlFor="bill-amount">
-        <h3 className="text-dark-grayish-cyan font-semibold">Bill</h3>
-        <div className="relative">
-          <input
-            className="relative w-full p-4 bg-very-light-gray-cyan mt-2 rounded-md focus:outline-hover-cyan active:outline-hover-cyan text-right text-very-dark-cyan font-semibold text-2xl"
-            type="number"
-            value={billAmount}
-            id="bill-amount"
-            placeholder="0.00"
-            onChange={(e) => setBill(e.target.value)}
-          />
-          <img
-            className="absolute top-[34px] left-4"
-            src={dollar}
-            alt="dollar sign"
-          />
-        </div>
-      </label>
+      <span className="text-dark-grayish-cyan font-semibold">
+        Bill
+      </span>
+      <div className="relative">
+        <label htmlFor="bill-amount"></label>
+        <input
+          className="relative w-full p-4 bg-very-light-gray-cyan mt-2 rounded-md focus:outline-hover-cyan active:outline-hover-cyan text-right text-very-dark-cyan font-semibold text-2xl"
+          type="number"
+          value={billAmount}
+          id="bill-amount"
+          placeholder="0.00"
+          onChange={(e) => setBill(e.target.value)}
+        />
+        <img
+          className="absolute top-[34px] left-4"
+          src={dollar}
+          alt="dollar sign"
+        />
+      </div>
     </form>
   );
 };
