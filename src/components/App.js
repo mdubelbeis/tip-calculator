741328ec6532a106f63bb024555349fede8e3a55
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-29 23:02:29 -0400

    styled header

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,9 @@ import Output from './Output';
 
 const App = () => {
   return (
-    <div>
-      <header>
-        <div>
+    <div className="flex flex-col">
+      <header className="p-10 text-center font-semibold tracking-[8px]">
+        <div className="text-very-dark-cyan">
           <p>SPLI</p>
           <p>TTER</p>
         </div>
