0fc2b4cac2aecf3d0c3af0e818f0f77001410335
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-29 23:42:35 -0400

    added custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
         'dark-grayish-cyan': 'hsl(186, 14%, 43%)',
         'grayish-cyan': 'hsl(184, 14%, 56%)',
         'light-grayish-cyan': 'hsl(185, 41%, 84%)',
-        'very-light- gray - cyan': 'hsl(189, 41%, 97%)',
+        'very-light-gray-cyan': 'hsl(189, 41%, 97%)',
         'pure-white': 'hsl(0, 0%, 100%)',
       },
       fontFamily: {
