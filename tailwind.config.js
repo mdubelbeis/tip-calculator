3d00badd123fdf6ed13a25f491937cbd32d140fb
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-29 23:02:46 -0400

    added custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,19 @@ module.exports = {
   content: ['./src/**/*.{js,jsx}', './public/index.html'],
   theme: {
     extend: {
-      'strong-cyan': 'hsl(172, 67%, 45%)',
-      'very-dark-cyan': 'hsl(183, 100%, 15%)',
-      'dark-grayish-cyan': 'hsl(186, 14%, 43%)',
-      'grayish-cyan': 'hsl(184, 14%, 56%)',
-      'light-grayish-cyan': 'hsl(185, 41%, 84%)',
-      'very-light- gray - cyan': 'hsl(189, 41%, 97%)',
-      'pure-white': 'hsl(0, 0%, 100%)',
-    },
-    fontFamily: {
-      'space-mono': ['Space Mono', 'monospace'],
+      colors: {
+        'strong-cyan': 'hsl(172, 67%, 45%)',
+        'light-blue': '#C5E4E7',
+        'very-dark-cyan': 'hsl(183, 100%, 15%)',
+        'dark-grayish-cyan': 'hsl(186, 14%, 43%)',
+        'grayish-cyan': 'hsl(184, 14%, 56%)',
+        'light-grayish-cyan': 'hsl(185, 41%, 84%)',
+        'very-light- gray - cyan': 'hsl(189, 41%, 97%)',
+        'pure-white': 'hsl(0, 0%, 100%)',
+      },
+      fontFamily: {
+        'space-mono': ['Space Mono', 'monospace'],
+      },
     },
   },
   plugins: [],
