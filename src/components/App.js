02842f9311349ba2a8f578ee59e2d3a09b5728e4
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-30 14:01:14 -0400

    added h1

--- src/components/App.js
+++ src/components/App.js
@@ -55,13 +55,15 @@ const App = () => {
 
   return (
     <div className="flex flex-col h-screen items-center xl:justify-center">
-      <header className="p-8 text-center font-semibold tracking-[8px] xl:pb-16">
-        <div className="text-very-dark-cyan text-2xl">
-          <p>SPLI</p>
-          <p>TTER</p>
+      <header className="p-8 text-center font-semibold tracking-[8px] xl:pt-0 xl:pb-16">
+        <div className="text-very-dark-cyan text-4xl">
+          <h1 className="flex flex-col">
+            <span>SPLI</span>
+            <span>TTER</span>
+          </h1>
         </div>
       </header>
-      <main className="w-screen p-8 bg-pure-white rounded-t-3xl max-w-[500px] xl:grid xl:grid-cols-2 xl:gap-10 xl:max-w-[920px] xl:max-h-[481px]">
+      <main className="w-screen p-8 bg-pure-white rounded-t-3xl max-w-[500px] xl:grid xl:grid-cols-2 xl:gap-10 xl:max-w-[920px] xl:max-h-[481px] xl:rounded-3xl">
         <Input
           setBill={setBill}
           setGuests={setGuests}
