25f4d575b0fd76566a046748cdf62b82b25f0cb2
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-30 00:07:11 -0400

    set max-w

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,14 @@ import Output from './Output';
 
 const App = () => {
   return (
-    <div className="flex flex-col">
-      <header className="p-10 text-center font-semibold tracking-[8px]">
-        <div className="text-very-dark-cyan">
+    <div className="flex flex-col h-screen items-center">
+      <header className="p-8 text-center font-semibold  tracking-[8px]">
+        <div className="text-very-dark-cyan text-xl">
           <p>SPLI</p>
           <p>TTER</p>
         </div>
       </header>
-      <main className="w-screen h-screen p-8 bg-pure-white rounded-t-3xl">
+      <main className="w-screen h-screen p-8 bg-pure-white rounded-t-3xl max-w-[500px]">
         <Input />
         <Output />
       </main>
